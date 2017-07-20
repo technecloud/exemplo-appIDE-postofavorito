ba9ad09c2bc90a41e946d68288695c699719d20a
rodrigogregorioneri <rodrigogregorioneri>
2017-07-20 15:34:58 +0000

    Adicionando o client HTTP tabela Fipe

---
+++ src/main/java/app/entity/Marcas.java
@@ -0,0 +1,69 @@
+package app.entity;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Rodrigo Gregorio Neri
+ * @version 1.0
+ * @since 2017-07-20
+ *
+ */
+ 
+public class Marcas {
+
+	private  String name;
+	private  String fipe_name;
+	private  int order;
+	private  String key;
+	private  int id;
+
+	public Marcas(String name, String fipe_name, int order, String key, int id) {
+	  this.name = name;
+	  this.fipe_name = fipe_name;
+	  this.order = order;
+	  this.key = key;
+	  this.id = id;
+	  
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFipe_name() {
+		return fipe_name;
+	}
+
+	public void setFipe_name(String fipe_name) {
+		this.fipe_name = fipe_name;
+	}
+
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
