5af924c255050f18eb7cbd67e8d7980351bb2bfc
rodrigogregorioneri <rodrigogregorioneri>
2017-08-01 20:19:12 +0000

    Retirando comentarios e mensagens no console

--- src/main/java/app/business/MarcasBusiness.java
+++ src/main/java/app/business/MarcasBusiness.java
@@ -22,21 +22,11 @@ import retrofit2.Retrofit;
 
 @Service("MarcasBusiness")
 public class MarcasBusiness {
-
-
-
-	public MarcasBusiness() {
-
-	}
-
+  
 	public List<Marcas> listarTodasMarcas( ) {
-	//	RetrofitBuilderInterface retrofit = RetrofitBuilderImpl.getInstance();
 		Retrofit retrofit = new Retrofit.Builder().baseUrl(ServiceMarcas.API_URL)
 				.addConverterFactory(GsonConverterFactory.create()).build();
-		
 		List<Marcas> marcas = null;
-	//	ParceiroService ps = retrofit.getInstance(token).create(ParceiroService.class);
-	
 	  ServiceMarcas mc = retrofit.create(ServiceMarcas.class);
 		
 		Call<List<Marcas>> call = mc.listatodasmarcas();
@@ -46,17 +36,12 @@ public class MarcasBusiness {
 			response = call2.execute();
 			marcas = response.body();
 			if (marcas != null) {
-			/*	for (Marcas i : marcas)
-					i.setData(UtilitariosGerais.dataString(i.getData()));*/
-					System.out.println("Entrou");
-					System.out.println(marcas);
 					return marcas;
 			}
 			return marcas;
 		} catch (Exception e) {
 			  e.printStackTrace();
 		}
-
 		return marcas;
 	}
 
