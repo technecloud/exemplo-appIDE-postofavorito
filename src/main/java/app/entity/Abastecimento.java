d095a7d9442929dcaa0141b4dc728219ddf0d77a
vanros <vanessarodrigues.vj@gmail.com>
2017-08-01 14:29:43 -0300

    Update Abastecimento.java

--- src/main/java/app/entity/Abastecimento.java
+++ src/main/java/app/entity/Abastecimento.java
@@ -76,7 +76,10 @@ public class Abastecimento implements Serializable {
   /**
   * @generated
   */
- 
+  @ManyToOne
+  @JoinColumn(name="fk_combustivel", nullable = true, referencedColumnName = "id", insertable=true, updatable=true)
+  private Combustivel combustivel;
+  
   /**
    * Construtor
    * @generated
@@ -242,7 +245,19 @@ public class Abastecimento implements Serializable {
    * return combustivel
    * @generated
    */
+  public Combustivel getCombustivel(){
+    return this.combustivel;
+  }
   
+  /**
+   * Define combustivel
+   * @param combustivel combustivel
+   * @generated
+   */
+  public Abastecimento setCombustivel(Combustivel combustivel){
+    this.combustivel = combustivel;
+    return this;
+  }
   
   /**
    * @generated
